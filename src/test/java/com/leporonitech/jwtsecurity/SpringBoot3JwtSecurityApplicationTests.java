4b9071b3eea86fcac9da9cd1cf619dc5c91d8c58
Leporoni <leporoni@hotmail.com>
2023-01-04 15:55:32 -0300

    Create project and config aplication.yml

---
+++ src/test/java/com/leporonitech/jwtsecurity/SpringBoot3JwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leporonitech.jwtsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3JwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
